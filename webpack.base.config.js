6cef436bcd87f1634ab5b7035f7b2d64da8288f0
Skidanenkoandrey <skidanenkoandrey@gmail.com>
2020-09-09 18:47:39 +0300

    webpack setup

---
+++ webpack.base.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const PATHS = {
+    src: path.join(__dirname, './src'),
+    dist: path.join(__dirname, './dist'),
+    assets: 'assets/'
+}
+
+module.exports = {
+    externals: {
+        paths: PATHS
+    },
+    entry: {
+        app: PATHS.src
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/',
+        }, {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {sourceMap: true}
+                }, {
+                    loader: 'postcss-loader',
+                    options: {sourceMap: true, config: {path: 'src/js/postcss.config.js'}}
+                }
+            ]
+        }, {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {sourceMap: true}
+                }, {
+                    loader: 'postcss-loader',
+                    options: {sourceMap: true, config: {path: 'src/js/postcss.config.js'}}
+                }, {
+                    loader: 'sass-loader',
+                    options: {sourceMap: true}
+                },
+            ]
+        },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].css`,
+        })
+    ]
+}
